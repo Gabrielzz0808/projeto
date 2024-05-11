ba4d24c61f6969b0af7bd704105e6f4a7f770d6d
unknown <silvadesousagabriel13@gmail.com>
2024-05-11 13:17:33 -0300

    Adicionando Buscar por Id

--- src/main/java/com/estudos/Controles/UsuariosControle.java
+++ src/main/java/com/estudos/Controles/UsuariosControle.java
@@ -1,11 +1,13 @@
 package com.estudos.Controles;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.estudos.Modelos.UsuarioModelo;
@@ -21,6 +23,11 @@ public class UsuariosControle {
     public ResponseEntity<List<UsuarioModelo>> BuscarTodosUser(){
         List<UsuarioModelo> listar = usuarioServices.BuscarUsuario();
         return ResponseEntity.status(HttpStatus.OK).body(listar);
+    }
 
+    @GetMapping("/Usuarios/{id}")
+    public ResponseEntity<UsuarioModelo> BuscarPorId(@PathVariable UUID idUser){
+        UsuarioModelo listar = usuarioServices.BuscarPorId(idUser);
+        return ResponseEntity.status(HttpStatus.OK).body(listar);
     }
 }
--- src/main/java/com/estudos/Services/UsuarioServices.java
+++ src/main/java/com/estudos/Services/UsuarioServices.java
@@ -1,6 +1,8 @@
 package com.estudos.Services;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,10 +17,20 @@ public class UsuarioServices {
     private UsuarioRepositorio usuarioRepositorio;
     
     /**
-     * Método para Buscar um Usuario
+     * Método para Buscar Todos Usuarios
      * @return Todos Usuarios
      */
     public List<UsuarioModelo> BuscarUsuario(){
         return usuarioRepositorio.findAll();
+    }
+
+    /**
+     * Método para Buscar Usuario pelo Id
+     * @param idUser
+     * @return Retorna Usuario id
+     */
+    public UsuarioModelo BuscarPorId(UUID idUser){
+        Optional<UsuarioModelo> obj = usuarioRepositorio.findById(idUser);
+        return obj.get();
     }
 }
